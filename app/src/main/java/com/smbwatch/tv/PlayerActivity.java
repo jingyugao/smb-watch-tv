c5b36e51219fc2732517ff83044c8426ede51df6
gaojingyu <1121087373@qq.com>
2026-07-13 21:43:46 +0800

    fix: remove title bars across TV screens

--- app/src/main/java/com/smbwatch/tv/PlayerActivity.java
+++ app/src/main/java/com/smbwatch/tv/PlayerActivity.java
@@ -10,6 +10,7 @@ import android.os.Looper;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -128,6 +129,7 @@ public class PlayerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_player);
         enterImmersiveMode();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
